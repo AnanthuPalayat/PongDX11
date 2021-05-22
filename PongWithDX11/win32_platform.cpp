ebf2826f72b0960290f6ecc630a8138b6d9a9597
ananthu palayat <ananthupalayat85@gmail.com>
2021-05-23 00:16:00 +0530

    Created A window

--- PongWithDX11/win32_platform.cpp
+++ PongWithDX11/win32_platform.cpp
@@ -1,15 +1,16 @@
 #include <Windows.h>
 
-bool isRunning = true;
+bool running = true;
 LRESULT CALLBACK window_callback(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam){
 	LRESULT result = 0;
 	switch(uMsg) {
 		case WM_CLOSE:
-		case WM_DESTROY:
-		isRunning = false;
-				break;
-		default: result = window_callback(hwnd, uMsg, wParam, lParam);
-			break;
+		case WM_DESTROY: {
+			running = false;
+		} break;
+		default: {
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		} break;
 	}
 	return result;
 }
@@ -20,7 +21,7 @@ int  WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nSh
 {
 	//Create Window Class
 	WNDCLASS window_class = {};
-	window_class.style = HS_HORIZONTAL | HS_VERTICAL;
+	window_class.style = CS_HREDRAW | CS_VREDRAW;
 	window_class.lpszClassName = "Window Class";
 	window_class.lpfnWndProc = &window_callback;
 
@@ -28,5 +29,18 @@ int  WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nSh
 	RegisterClass(&window_class);
 
 	//Create a window
+	HWND window=CreateWindow(window_class.lpszClassName, "My First Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+
+	while (running) {
+		//Input
+		MSG message;
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+		//Simulate
+
+		//Render
+	}
 
 }
